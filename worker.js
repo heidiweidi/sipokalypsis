2804692ce8f1a4cc34d5b65718728a73cdcd1ccf
heidiweidi <heidi.marasigan@gmail.com>
2025-08-16 21:13:31 +0800

    Add assets folder

--- worker.js
+++ worker.js
@@ -41,41 +41,56 @@ export default {
         <!DOCTYPE html>
         <html lang="en">
         <head>
-          <meta charset="UTF-8" />
-          <title>Word of Life</title>
-          <style>
-            body {
-              font-family: 'Segoe UI', sans-serif;
-              background: #222222;
-              color: #333;
-              text-align: center;
-              padding: 50px;
-              margin: auto 0;
-            }
-            .verse-box {
-              background: #fff;
-              padding: 30px;
-              border-radius: 10px;
-              box-shadow: 0 0 10px rgba(0,0,0,0.1);
-              max-width: 600px;
-              margin: 0 auto;
-            }
-            #verse-text {
-              font-size: 1.5em;
-              margin-bottom: 20px;
-            }
-            #verse-ref {
-              font-size: 1em;
-              font-weight: bold;
-              color: #555;
-            }
-          </style>
+          <title>Landed by HTML5 UP</title>
+          <meta charset="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+          <link rel="stylesheet" href="assets/css/main.css" />
+          <noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
         </head>
-        <body>
-          <div class="verse-box">
-            <div id="verse-text">${data.text.trim().replace(/\n/g, "<br>")}</div>
-            <div id="verse-ref">${data.reference}</div>
+        <body class="is-preload landing">
+          <div id="page-wrapper">
+            <!-- Header -->
+            <header id="header">
+              <h1 id="logo"><a href="index.html">Landed</a></h1>
+            </header>
+
+            <!-- Banner -->
+            <section id="banner">
+              <div class="content">
+                <header>
+                  <h2>${data.text.trim().replace(/\n/g, "<br>")}</h2>
+                  <p>${data.reference}</p>
+                </header>
+                <span class="image"><img src="images/pic01.jpg" alt="" /></span>
+              </div>
+              <a href="#one" class="goto-next scrolly">Next</a>
+            </section>
+
+            <!-- Footer -->
+            <footer id="footer">
+              <ul class="icons">
+                <li><a href="#" class="icon brands alt fa-twitter"><span class="label">Twitter</span></a></li>
+                <li><a href="#" class="icon brands alt fa-facebook-f"><span class="label">Facebook</span></a></li>
+                <li><a href="#" class="icon brands alt fa-linkedin-in"><span class="label">LinkedIn</span></a></li>
+                <li><a href="#" class="icon brands alt fa-instagram"><span class="label">Instagram</span></a></li>
+                <li><a href="#" class="icon brands alt fa-github"><span class="label">GitHub</span></a></li>
+                <li><a href="#" class="icon solid alt fa-envelope"><span class="label">Email</span></a></li>
+              </ul>
+              <ul class="copyright">
+                <li>&copy; Untitled. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+              </ul>
+            </footer>
           </div>
+
+          <!-- Scripts -->
+          <script src="assets/js/jquery.min.js"></script>
+          <script src="assets/js/jquery.scrolly.min.js"></script>
+          <script src="assets/js/jquery.dropotron.min.js"></script>
+          <script src="assets/js/jquery.scrollex.min.js"></script>
+          <script src="assets/js/browser.min.js"></script>
+          <script src="assets/js/breakpoints.min.js"></script>
+          <script src="assets/js/util.js"></script>
+          <script src="assets/js/main.js"></script>
         </body>
         </html>
       `;
